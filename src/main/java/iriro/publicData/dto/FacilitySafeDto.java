da4f27d82b4cc03f98e3b45a66d703d05e6745a4
leehyeon2021 <leehyeon2021@gmail.com>
2026-03-23 16:01:45 +0900

    260323 entity dto 생성

--- src/main/java/iriro/publicData/dto/FacilitySafeDto.java
+++ src/main/java/iriro/publicData/dto/FacilitySafeDto.java
@@ -1,4 +1,27 @@
 package iriro.publicData.dto;
 
+import iriro.publicData.entity.FacilitySafeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data@NoArgsConstructor@AllArgsConstructor@Builder
 public class FacilitySafeDto {
+    private Integer facId;
+    private String facType;
+    private String facSgg;
+    private String facName;
+    private String facAdd;
+    private Double facLat;
+    private Double facLng;
+    private Integer facCount;
+    private String facUse;
+    private String facTel;
+
+    public FacilitySafeEntity toEntity(){
+        return FacilitySafeEntity.builder()
+                .facId(this.facId).facType(this.facType).facSgg(this.facSgg).facName(this.facName).facAdd(this.facAdd).facLat(this.facLat).facLng(this.facLng).facCount(this.facCount).facUse(this.facUse).facTel(this.facTel)
+                .build();
+    }
 }
--- src/main/java/iriro/publicData/entity/CrimeRoadEntity.java
+++ src/main/java/iriro/publicData/entity/CrimeRoadEntity.java
@@ -1,5 +1,6 @@
 package iriro.publicData.entity;
 
+import iriro.publicData.dto.CrimeRoadDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,17 +11,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor @AllArgsConstructor
 @Data @Builder
 public class CrimeRoadEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cri_id")
     private Integer criId;
-
     @Column(name = "cri_zip")
     private Integer criZip;
-
     @Column(name = "cri_sgg", length = 50)
     private String criSgg;
-
     @Column(name = "cri_road", length = 300)
     private String criRoad;
+
+    public CrimeRoadDto toDto(){
+        return CrimeRoadDto.builder()
+                .criId(this.criId).criZip(this.criZip).criSgg(this.criSgg).criRoad(this.criRoad)
+                .build();
+    }
+
+
 }
