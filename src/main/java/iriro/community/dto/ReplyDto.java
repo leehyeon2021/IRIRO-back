d027ea039bbd0c2bb74806a643b70b2903007c6b
so0hub <so0mile02@gmail.com>
2026-03-23 17:16:20 +0900

    박소영 5:16

--- src/main/java/iriro/community/dto/ReplyDto.java
+++ src/main/java/iriro/community/dto/ReplyDto.java
@@ -1,5 +1,6 @@
 package iriro.community.dto;
 
+import iriro.community.entity.ReplyEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,4 +11,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor // 롬복
 public class ReplyDto {
+    private Integer replyId; // 댓글번호
+    private Integer userId; // 회원번호
+    private Integer boardId; // 글 번호
+    private String replyContent; // 내용
+    private String createdAt;
+    private String updatedAt;
+
+    // + Dto --> Entity 변환 // 사용자가 직접 입력한 데이터만.
+    public ReplyEntity replyEntity(){
+        return ReplyEntity.builder()
+                .replyContent(this.replyContent)
+                .build();
+    }
 }
--- src/main/java/iriro/community/entity/BoardEntity.java
+++ src/main/java/iriro/community/entity/BoardEntity.java
@@ -1,6 +1,8 @@
 package iriro.community.entity;
 
+import iriro.common.entity.BaseTime;
 import iriro.community.dto.BoardDto;
+import iriro.saferoute.entity.LocationlogEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,6 +34,7 @@ public class BoardEntity extends BaseTime {
     @JoinColumn( name = "user_id")
     private UserEntity userEntity;
 
+    // 로그 번호
     @ManyToOne
     @JoinColumn( name = "log_id")
     private LocationlogEntity LocationlogEntity;
