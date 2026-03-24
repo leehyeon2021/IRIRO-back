19175c07a662e9de4984559fa830f790ad1654e0
leehyeon2021 <leehyeon2021@gmail.com>
2026-03-24 17:23:00 +0900

    260324 serviceKey 저장

--- src/main/java/iriro/publicData/service/CrimeRoadFetchService.java
+++ src/main/java/iriro/publicData/service/CrimeRoadFetchService.java
@@ -1,27 +1,28 @@
 package iriro.publicData.service;
 
+import iriro.publicData.repository.CrimeRoadRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Map;
+import java.util.Objects;
 
 @Service @RequiredArgsConstructor
 public class CrimeRoadFetchService{
 
-    // API 인증키
-    String serviceKey = "L59Y0SCX-L59Y-L59Y-L59Y-L59Y0SCX4Q";
-    // web 요청 객체
+    @Value("${api.pub.service-key}")
+    private String serviceKey;
+
+    @Value("${api.pub.crime-road.url}")
+    private String crimeRoadUrl;
+
     private final WebClient webClient = WebClient.builder().build();
+    private final CrimeRoadRepository cr;
 
     // (공공데이터 수집) 범죄자도로명
-    public Map<String,Object> fetchCrimeRoad(){ // 원래 boolean. test 중
-        String uri = "https://apis.data.go.kr/1383000/sais/SexualAbuseNoticeAddrService";
-        uri+= "?serviceKey="+serviceKey;
-        for(int i=1;i<) {
-            uri +=
-        }
-    }
+
 
 
 }
--- src/main/java/iriro/publicData/service/FacilitySafeFetchService.java
+++ src/main/java/iriro/publicData/service/FacilitySafeFetchService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 
 @Service @RequiredArgsConstructor
 public class FacilitySafeFetchService {
-    String pubServiceKey = "b9c001aab838aaf168743c5eea9b492c8651a999fc31922d4edbd67c95035ea5";
-    String seoulServiceKey = "66666d56446c656533386572676756";
+
+
 }
