5a6c1f9fcacf87bc3268fdb92f1c6cd0472438f3
bm1611 <bm1611@naver.com>
2026-03-24 21:29:09 +0900

    군집화 코드 생성

--- src/main/java/iriro/saferoute/service/RiskPointFilterService.java
+++ src/main/java/iriro/saferoute/service/RiskPointFilterService.java
@@ -13,40 +13,28 @@ import java.util.List;
 public class RiskPointFilterService {
     private static final double EARTH_RADIUS = 6371000; // 지구 반지름(상수)
 
-    public List<DetourWayPointDto> getDetourWayPoint(List<RoutePointDto> routePoints, BboxDto bbox){
-
-        // 위험 지역을 DB에서 조사해 1차 필터링을 거친 값들만 리스트로 변환하여 가져옴. --> 추후 JPA활용하여 처리
-        List<RoutePointDto> dangerPoints = TestSampleCode.dangerRoutePoints;
-
-        List<RoutePointDto> firstInDangerPoints = dangerPoints.stream().filter(point ->
-                        isInsideBbox(point.getLat().doubleValue(), point.getLng().doubleValue(),bbox))
-                .toList();
+    // 우회 경유지 리스트 반환하는 함수
+    public List<DetourWayPointDto> getDetourWayPoint(List<RoutePointDto> routePoints,List<RoutePointDto> firstInDangerPoints){
 
         // 필터링된 위험구역 지점들을 2차 필터(경로 마다 50m 안에 들어오는 위험 지점들만 리스트화)
         List<RoutePointDto> secondInDangerPoints = firstInDangerPoints.stream().filter(point ->
                 getMinDistance(routePoints, point) <= 50 ).toList(); // 50m 이내의 점들만 리스트에 추가
 
-
         // 우회 경유지 리스트 생성
-        List<RoutePointDto> detourWaypoints = new ArrayList<>();
+        List<DetourWayPointDto> detourWaypoints = new ArrayList<>();
         // 경로상 위험지역 포함 유무에 따라 다르게 처리
         if(!secondInDangerPoints.isEmpty()){ // 경로 상에 위험지역이 있으면...
             for(RoutePointDto riskPoint : secondInDangerPoints){ // 경로에 걸치는 위험지역마다 우회경로 포인트 생성
                 int sequence = getSequence(routePoints, riskPoint); // 순서가져오기
-                // 위험지역이 겹치면? 위험지역이 처음 출발지라면? 너무나도 if 조건이 많다;
+
+                if(sequence == 1) continue; // 첫번째 위치일 경우 무시
+
+
             }
         }else{ // 경로 상에 위험지역이 없다면 기존 경로 안전점수 계산하여 반환
 
         }
-        return null;
-    }
-
-    // Bbox안에 있는지 체크하는 함수
-    public boolean isInsideBbox(double lat, double lng, BboxDto bbox){
-        return lat >= bbox.getMinLat()
-                && lat <= bbox.getMaxLat()
-                && lng >= bbox.getMinLng()
-                && lng <= bbox.getMaxLng();
+        return detourWaypoints;
     }
 
     // 위험 좌표 1개당 경로 좌표들 중 최소거리와 순서를 구하는 함수
@@ -101,4 +89,7 @@ public class RiskPointFilterService {
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         return EARTH_RADIUS * c;
     }
+
+    // 클러스트링, 군집화 함수 만들기
+
 }
