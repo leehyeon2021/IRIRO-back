27ea0f3fe983cbb38678cb7158c5674e997f287d
so0hub <so0mile02@gmail.com>
2026-03-25 17:41:08 +0900

    로그인, 로그아웃

--- src/main/java/iriro/community/controller/UserController.java
+++ src/main/java/iriro/community/controller/UserController.java
@@ -1,14 +1,17 @@
 package iriro.community.controller;
 
 import iriro.community.dto.UserDto;
+import iriro.community.entity.ReplyEntity;
+import iriro.community.entity.UserEntity;
+import iriro.community.repository.UserRepository;
 import iriro.community.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,12 +20,36 @@ public class UserController {
 
     @Autowired // 주방장을 불러오거라
     private UserService userService;
+    private UserRepository userRepository;
 
     // 1. 회원가입 post = create = save
     @PostMapping("/join")
     // http://localhost:8080/user/join
     // { "email" : "soso@sadsa.com","pwToken" : "1234","nickName" : "박진감"}
-    public ResponseEntity<?> join(@RequestBody UserDto userDto){
-        return ResponseEntity.ok( userService.join(userDto));
+    public ResponseEntity<?> join(@RequestBody UserDto joinDto) {
+        return ResponseEntity.ok(userService.join(joinDto));
+    }
+
+    // 2. 로그인 Get
+    @PostMapping("/login")
+    // http://localhost:8080/user/login
+    // { "email" : "soso@sadsa.com","pwToken" : "1234" }
+    public ResponseEntity<?> login(@RequestBody UserDto loginDto, HttpSession session) {
+        // 1] 입력받은 아이디/비밀번호를 서비스에게 보낸다.
+        boolean result = userService.login(loginDto);
+        if (result) {// 1) 매개변수에 HttpSession session 받는다.
+            // 2) 로그인 성공한 회원의 아이디를 세션 객체내 저장 , .setAttribute("속성명",속성값);
+            session.setAttribute("email", loginDto.getEmail());
+        }
+        // 3] 아니면 실패
+        return ResponseEntity.ok(result);
+    }
+
+    // 3. 로그아웃 Get
+    @GetMapping("/logout")
+    // http://localhost:8080/user/logout
+    public ResponseEntity<?> logout(HttpSession session){ // 1) 매개변수에 HttpSession session 받는다.
+        session.removeAttribute("email");
+        return ResponseEntity.ok(true);
     }
 }
--- src/main/java/iriro/community/service/UserService.java
+++ src/main/java/iriro/community/service/UserService.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -18,15 +20,33 @@ public class UserService {
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     // 1. 회원가입
-    public boolean join(UserDto userDto){
+    public boolean join(UserDto joinDto){
         // dto --> entity 변환
-        UserEntity saveEntity = userDto.toEntity();
+        UserEntity saveEntity = joinDto.toEntity();
         // **** 최종 저장하기 전에 입력받은 비밀번호를 암호화 ****  passwordEncoder : 분쇄기
         String pwd = passwordEncoder.encode(saveEntity.getPwToken());
         saveEntity.setPwToken(pwd);
         UserEntity savedEntity = userRepository.save(saveEntity);
         if(savedEntity.getUserId() > 0){return true;}
         return false;
+    }
+
+    // 2. 로그인
+    public boolean login(UserDto loginDto){
+        // 1] JPA으로 아이디로 엔티티찾기 , SQL로 아이디/비밀번호 일치 여부로 판단 불가능
+        Optional<UserEntity> optionalUser = userRepository.findByEmail(loginDto.getEmail());
+        // 2] 만약에 조회된 엔티티가 존재하면
+        if(optionalUser.isPresent()){
+            // 엔티티 꺼내기
+            UserEntity userEntity = optionalUser.get();
+            // 비크립트 암호화로 평문과 암호화문 비교, passwordEncoder.matches( 평문 , 암호문 );
+            boolean result = passwordEncoder.matches(loginDto.getPwToken(), userEntity.getPwToken());
+            if( result == true ){ return true; } // 로그인 성공
+            else{ return false; } // 로그인 실패(패스워드 다를 때)
+        }
+        // 3] 없으면 로그인 실패(아이디 없을 때)
+        return false;
+
     }
 
 }
