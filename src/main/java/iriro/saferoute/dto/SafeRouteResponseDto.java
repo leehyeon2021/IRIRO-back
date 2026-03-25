6fd94edde5ed430200f56628378dc8e24e087db3
bm1611 <bm1611@naver.com>
2026-03-25 17:38:04 +0900

    안전 경로 반환Dto(기본 경로에다 안전점수까지)

--- src/main/java/iriro/saferoute/dto/SafeRouteResponseDto.java
+++ src/main/java/iriro/saferoute/dto/SafeRouteResponseDto.java
@@ -10,17 +10,6 @@ import java.util.List;
 
 @NoArgsConstructor @AllArgsConstructor @Data @Builder
 public class SafeRouteResponseDto {
-    private BigDecimal start_latitude;
-    private BigDecimal start_longitude;
-    private BigDecimal end_latitude;
-    private BigDecimal end_longitude;
-
+    private RouteResponseDto detourRoute; //경유지 우회 경로
     private Integer safety_score;
-    private Integer totalTime; // 초 단위
-    private Integer totalDistance; // m 단위로 저장
-
-    private Integer rating;
-    private String createdAt;
-
-    private List<RoutePointDto> routePoints; // 경로가 들어가 있는 리스트
 }
