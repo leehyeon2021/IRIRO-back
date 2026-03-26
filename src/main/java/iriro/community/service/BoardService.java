2f3504dfe996cb8cf35c9927d3f06732f9705bed
so0hub <so0mile02@gmail.com>
2026-03-26 12:12:26 +0900

    [세션 연동] 리뷰 삭제 수정

--- src/main/java/iriro/community/service/BoardService.java
+++ src/main/java/iriro/community/service/BoardService.java
@@ -6,21 +6,18 @@ import iriro.community.dto.BoardDto;
 import iriro.community.entity.BoardEntity;
 import iriro.community.entity.UserEntity;
 import iriro.community.repository.BoardRepository;
-import iriro.community.repository.ReplyRepository;
+
 import iriro.community.repository.UserRepository;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.servlet.http.HttpSession;
+
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-import static aQute.bnd.annotation.headers.Resolution.optional;
 
 @Service
 @RequiredArgsConstructor
@@ -28,16 +25,14 @@ import static aQute.bnd.annotation.headers.Resolution.optional;
 public class BoardService {
     private final BoardRepository boardRepository;
     private final UserRepository userRepository;
-    private final ReplyRepository replyRepository;
-    private final BoardService boardService;
 
     // 1. 리뷰 등록 (회원)
-    public boolean rvAdd( BoardDto boardDto , String email ) {
+    public boolean rvAdd(BoardDto boardDto, String loginEmail) {
         // 1] dto --> entity 변환
         BoardEntity saveEntity = boardDto.toEntity();
         // ********* 저장하기 전에 FK 대입하기 , FK의 엔티티를 찾아서 대입 ***********
         // 현재 로그인 중인 email로 엔티티 찾기
-        Optional<UserEntity> entityOptional = userRepository.findByEmail(email);
+        Optional<UserEntity> entityOptional = userRepository.findByEmail(loginEmail);
         if (!entityOptional.isPresent()) { // !부정문 , isPresent() 아니면
             return false; // 존재하지 않은 회원으로 실패
         }
@@ -53,7 +48,7 @@ public class BoardService {
     }
 
     // 2. 리뷰 전체 조회
-    public List<BoardDto> rvAllView(){
+    public List<BoardDto> rvAllView() {
         // 1. DB(창고)에서 모든 엔티티(생고기 박스) 꺼내오기.
         List<BoardEntity> entityList = boardRepository.findAll();
 
@@ -61,7 +56,7 @@ public class BoardService {
         List<BoardDto> dtoList = new ArrayList<>();
 
         // 3. Entity --> Dto 변환 생고기박스에서 하나씩 꺼냄 앤나 접시에 옮김
-        for(BoardEntity entity : entityList){
+        for (BoardEntity entity : entityList) {
             BoardDto dto = entity.toDto(); // 생고기를 접시에 플레이팅함
 
             // 접시를 새박스에 쌓는다.
@@ -71,19 +66,29 @@ public class BoardService {
     }
 
     // 3. 리뷰 상세 조회
-    public BoardDto rvView(Integer boardId){
+    public BoardDto rvView(Integer boardId) {
         BoardEntity entity = boardRepository.findById(boardId).orElse(null);
         return entity.toDto(); // 엔티티 --> 디티오
     }
 
     // 4. 리뷰 개별 삭제 (회원)
-    public boolean rvDelete(Integer boardId ,
+    public boolean rvDelete(Integer boardId, String loginEmail) {
+        Optional<BoardEntity> boardOptinal = boardRepository.findById(boardId);
+        if (boardOptinal.isPresent()) {
+            BoardEntity board = boardOptinal.get();
+            if (board.getUserEntity().getEmail().equals(loginEmail)) {
+                boardRepository.deleteById(boardId);
+                return true;
+            }
+        }
+        return false;
+    }
 
     // 5. 글 추천
-    public boolean ddabong(Integer boardId){
+    public boolean ddabong(Integer boardId) {
         Optional<BoardEntity> optionalBoard = boardRepository.findById(boardId);
 
-        if(optionalBoard.isPresent()) {
+        if (optionalBoard.isPresent()) {
             BoardEntity board = optionalBoard.get();
 
             board.setRecommendCount(board.getRecommendCount() + 1);
@@ -92,6 +97,4 @@ public class BoardService {
         }
         return false;
     }
-
-    }
-
+}
