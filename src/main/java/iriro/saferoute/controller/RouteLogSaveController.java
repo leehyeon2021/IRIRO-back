77b5322a41070905303b96e0340a40c4cbfeb119
bm1611 <bm1611@naver.com>
2026-03-26 13:03:42 +0900

    안전경로로그 저장 컨트롤러

--- src/main/java/iriro/saferoute/controller/RouteLogSaveController.java
+++ src/main/java/iriro/saferoute/controller/RouteLogSaveController.java
@@ -3,13 +3,21 @@ package iriro.saferoute.controller;
 
 import iriro.saferoute.service.RouteLogSaveService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/savelog")
+@RequestMapping("/api")
 public class RouteLogSaveController {
     // 사용자가 후기를 남겨주면 로그를 저장하는 컨트롤러
     private final RouteLogSaveService routeLogSaveSvc;
+
+    // 후기 저장
+    @PostMapping("/savelog")
+    public ResponseEntity<?> saveLog(){
+        return ResponseEntity.ok( true );
+    }
 }
