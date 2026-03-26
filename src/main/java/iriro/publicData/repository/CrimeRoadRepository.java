e62f72f1dc73abff710d71455caa3ea7891c656e
bm1611 <bm1611@naver.com>
2026-03-26 15:56:21 +0900

    범위 내 조회하는 함수 추가

--- src/main/java/iriro/publicData/repository/CrimeRoadRepository.java
+++ src/main/java/iriro/publicData/repository/CrimeRoadRepository.java
@@ -2,7 +2,22 @@ package iriro.publicData.repository;
 
 import iriro.publicData.entity.CrimeRoadEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
-public interface CrimeRoadRepository extends JpaRepository<CrimeRoadEntity,Integer>{ }
+public interface CrimeRoadRepository extends JpaRepository<CrimeRoadEntity,Integer>{
+
+    // 위험 테이블 범위 기준 조회
+    @Query( value = "select * from crime_road r where r.latitude between :minLat and :maxLat and r.longitude between :minLng and :maxLng",
+            nativeQuery = true)
+    List<CrimeRoadEntity> findAllInBoundingBox(
+            @Param("minLat") BigDecimal minLat, @Param("maxLat") BigDecimal maxLat,
+            @Param("minLng") BigDecimal minLng, @Param("maxLng") BigDecimal maxLng
+    );
+
+}
--- src/main/java/iriro/publicData/repository/FacilitySafeRepository.java
+++ src/main/java/iriro/publicData/repository/FacilitySafeRepository.java
@@ -1,12 +1,24 @@
 package iriro.publicData.repository;
 
+import iriro.publicData.entity.CrimeRoadEntity;
 import iriro.publicData.entity.FacilitySafeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
 public interface FacilitySafeRepository extends JpaRepository<FacilitySafeEntity,Integer>{
     List<FacilitySafeEntity> findByFacType( String facType );
+
+    // 안전 테이블 범위 기준 조회
+    @Query( value = "select * from facility_safe r where r.latitude between :minLat and :maxLat and r.longitude between :minLng and :maxLng",
+            nativeQuery = true)
+    List<FacilitySafeEntity> findAllInBoundingBox(
+            @Param("minLat") BigDecimal minLat, @Param("maxLat") BigDecimal maxLat,
+            @Param("minLng") BigDecimal minLng, @Param("maxLng") BigDecimal maxLng
+    );
 }
