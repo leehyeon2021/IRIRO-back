8f7bb37a7d2caa3b26e1e1d026ef9a02df0c2967
leehyeon2021 <leehyeon2021@gmail.com>
2026-03-26 17:03:05 +0900

    260326 업데이트(저장/삭제)용 추가

--- src/main/java/iriro/publicData/repository/FacilitySafeRepository.java
+++ src/main/java/iriro/publicData/repository/FacilitySafeRepository.java
@@ -2,11 +2,18 @@ package iriro.publicData.repository;
 
 import iriro.publicData.entity.FacilitySafeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FacilitySafeRepository extends JpaRepository<FacilitySafeEntity,Integer>{
-    List<FacilitySafeEntity> findByFacType( String facType );
+
+    // 저장(=업데이트) 사용 (시설 이름, 주소)
+    Optional<FacilitySafeEntity> findByFacNameAndFacAdd(String facName , String facAdd);
+    // 삭제(=업데이트) 사용
+    List<FacilitySafeEntity> findByFacType(String facType);
+
 }
