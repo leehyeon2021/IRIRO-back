01c0b7f0ccbe8c1652504d521619f4d6f1800b63
leehyeon2021 <leehyeon2021@gmail.com>
2026-03-26 13:04:31 +0900

    260326 test controller 추가

--- src/main/java/iriro/publicData/controller/TestController.java
+++ src/main/java/iriro/publicData/controller/TestController.java
@@ -13,9 +13,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     private final CrimeRoadFetchService cf;
     private final FacilitySafeFetchService ff;
+
     // 저장 기능을 테스트하기 위한 컨트롤러입니다.
-    @GetMapping("/test1")
+
+    // 위험 도로
+    @GetMapping("/test1/cri")
     public ResponseEntity<?> test1(){
         return ResponseEntity.ok(cf.fetchCrimeRoad());
+    }
+
+    // 안심지킴이집
+    @GetMapping("/test2/safehouse")
+    public ResponseEntity<?> test2(){
+        return ResponseEntity.ok(ff.fetchSafeHouse());
+    }
+
+    // 경찰서(치안안전시설)
+    @GetMapping("/test2/police")
+    public ResponseEntity<?> test3(){
+        return ResponseEntity.ok(ff.fetchPoliceStation());
+    }
+
+    // 안전시설물
+    @GetMapping("/test2/safefac")
+    public ResponseEntity<?> test4(){
+        return ResponseEntity.ok(ff.fetchSafeFac());
     }
 }
