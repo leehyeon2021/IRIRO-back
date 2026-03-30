827cf743597d734e83e73135b4e528dc0bf96c94
bm1611 <bm1611@naver.com>
2026-03-30 17:30:40 +0900

    getSafeRoute함수 분리 -> buildresponse(), calcSafetyScore()

--- src/main/java/iriro/saferoute/service/SafeRouteService.java
+++ src/main/java/iriro/saferoute/service/SafeRouteService.java
@@ -1,5 +1,6 @@
 package iriro.saferoute.service;
 
+import iriro.common.exception.LogSaveException;
 import iriro.publicData.entity.CrimeRoadEntity;
 import iriro.publicData.entity.FacilitySafeEntity;
 import iriro.publicData.repository.CrimeRoadRepository;
@@ -22,6 +23,7 @@ public class SafeRouteService {
     private final RiskFilterService riskFilterSvc; // 위험 지역 필터 서비스
     private final SafeFacFilterService safeFacFilterSvc; // 안전 지역 필터 서비스
     private final DetourRouteService detourRouteSvc; // 우회 경로 서비스
+    private final RouteLogSaveService routeLogSaveSvc;
     private final CrimeRoadRepository crimeRoadRepository;
     private final FacilitySafeRepository facilitySafeRepository;
 
@@ -34,17 +36,17 @@ public class SafeRouteService {
             return null;
         }
 
-        double minLat = routePoints.get(0).getLatitude().doubleValue();
-        double maxLat = routePoints.get(0).getLatitude().doubleValue();
-        double minLng = routePoints.get(0).getLatitude().doubleValue();
-        double maxLng = routePoints.get(0).getLatitude().doubleValue();
+        double minLat = routePoints.get(0).getLatitude();
+        double maxLat = routePoints.get(0).getLatitude();
+        double minLng = routePoints.get(0).getLatitude();
+        double maxLng = routePoints.get(0).getLatitude();
 
         // 가장 크고 작은 위 경도 값 구하기
         for(RoutePointDto point : routePoints){
-            minLat = Math.min(minLat, point.getLatitude().doubleValue() );
-            minLng = Math.min(minLng, point.getLongitude().doubleValue() );
-            maxLat = Math.max(maxLat, point.getLatitude().doubleValue() );
-            maxLng = Math.max(maxLng, point.getLongitude().doubleValue() );
+            minLat = Math.min(minLat, point.getLatitude() );
+            minLng = Math.min(minLng, point.getLongitude() );
+            maxLat = Math.max(maxLat, point.getLatitude() );
+            maxLng = Math.max(maxLng, point.getLongitude() );
         }
 
         // 50m 정도 margin
@@ -61,9 +63,41 @@ public class SafeRouteService {
         );
     }
 
+    // 로그를 후기(평점)제외하고 저장하고 lodId를 반환하는 함수 --> API 명세서에 추가해야함
+    public Long saveRouteLog(RouteResponseDto originRoute, int safety_score ){
+
+        // SaveLogDto로 변환
+        SaveLogDto saveLog = originRoute.toSaveLogDto();
+        saveLog.setSafetyScore(safety_score); //안전 점수 추가
+
+        // 로그 1차 저장 후 로그 Id 받아오기
+        Long logId = routeLogSaveSvc.createRouteLog(saveLog);
+        // 로그Id 경로까지 저장 후 잘 저장되었는지 반환하기 ( 로그 아이디와, 경로 배열을 반환)
+        List<RoutePointDto> safeRoutePoints = originRoute.getRoutePoints();
+        boolean result = routeLogSaveSvc.saveLogRoute( logId, safeRoutePoints );
+
+        if(result) return logId; // 경로 저장까지 성공 시 로그아이디 반환
+        else return 0L; // 실패시 0L 반환
+    }
+
+    // 로그를 저장하고 빌드하여 응답객체를 반환
+    private SafeRouteResponseDto buildResponse(RouteResponseDto route, RouteResponseDto originRoute, int safetyScore){
+        Long logId = saveRouteLog(originRoute, safetyScore);
+        if (logId == 0L) { // 예외처리
+            throw new LogSaveException("log_id is 0L");
+        }
+
+        return SafeRouteResponseDto.builder()
+                .detourRoute(route)
+                .safety_score(safetyScore)
+                .logId(logId)
+                .build();
+    }
+
     // 안전 경로 계산 함수
     public SafeRouteResponseDto getSafeRoute(RouteRequestDto routeRequestDto){
 
+        int safety_score;
         // (출발지 위/경도, 목적지 위/경도, 경로 배열, 총걸린 시간, 총 거리)를 반환
         RouteResponseDto originRoute = tmapRouteSvc.getPedestrianRoute(routeRequestDto); // 경로 생성 API 1번 호출
         List<RoutePointDto> routePoints = originRoute.getRoutePoints(); // 기존 안내 경로
@@ -75,6 +109,7 @@ public class SafeRouteService {
                 BigDecimal.valueOf(bbox.getMinLat()), BigDecimal.valueOf(bbox.getMaxLat()),
                 BigDecimal.valueOf(bbox.getMinLng()), BigDecimal.valueOf(bbox.getMaxLng()))
                 .stream().map(FacilitySafeEntity::toSafetyFacPointDto).toList(); // 해당 엔티티를 SafetyFacPoint로 변경
+
         List<RiskPointDto> allDangerPoints = crimeRoadRepository.findAllInBoundingBox(
                 BigDecimal.valueOf(bbox.getMinLat()), BigDecimal.valueOf(bbox.getMaxLat()),
                 BigDecimal.valueOf(bbox.getMinLng()), BigDecimal.valueOf(bbox.getMaxLng()))
@@ -83,34 +118,26 @@ public class SafeRouteService {
         System.out.println("안전시설물Dto: " + allSafetyPoints);
         System.out.println("위험Dto: " + allDangerPoints);
 
-
-        // 안전 지역 1,2차 필터링 ( bbox, 경로상 50m 이내 )
-        List<SafetyFacPointDto> filteredSafetyFacPoints = safeFacFilterSvc.filterSafetyFacPoints(routePoints, allSafetyPoints);
-        // 위험 리스트 1, 2, 3차 필터링( bbox, 경로상 50m 이내, 연속된 위험지역 건너뛰기 )
-        List<RiskPointDto> filteredDangerPoints = riskFilterSvc.filterDangerPoints(routePoints, allDangerPoints);
-
-        System.out.println("필터링된 안전시설물Dto: " + filteredSafetyFacPoints);
-        System.out.println("필터링된 위험Dto: " + filteredDangerPoints);
-
+        List<RiskPointDto> riskFilterList = riskFilterSvc.filterDangerPoints(routePoints , allDangerPoints);
         // 만약 필터링 된 위험리스트가 비어있으면 기존 경로 안전점수 계산 후 반환
-        if(filteredDangerPoints.isEmpty()){
+        if( riskFilterList.isEmpty()){
             System.out.println("위험리스트가 비어있습니다. 기존경로를 반환합니다.");
             // 기존 경로 안전 점수
-            int safety_score = getSafetyScore(routePoints, filteredSafetyFacPoints, filteredDangerPoints);
-            return SafeRouteResponseDto.builder().detourRoute(originRoute).safety_score(safety_score).build();
+            safety_score = calcSafetyScore(routePoints, allSafetyPoints, allDangerPoints);
+            return buildResponse(originRoute, originRoute, safety_score); // 1차 기본경로 리턴
         }
 
         // 우회 경유지 목록 생성
-        List<DetourWayPointDto> detourPoints = detourRouteSvc.getDetourWayPoints(routePoints, filteredDangerPoints);
-        RouteResponseDto detourRoute = tmapRouteSvc.getDetourRoute(routeRequestDto, detourPoints); // ++추가 TmapAPI 호출
-        List<RoutePointDto> detourRoutePoints = detourRoute.getRoutePoints();
+        List<DetourWayPointDto> detourPoints = detourRouteSvc.getDetourWayPoints(routePoints, riskFilterList);
 
         // 우회 경유지가 없다면 기본 경로 반환
         if(detourPoints.isEmpty()){
             System.out.println("우회한 경유지가 없습니다. 기본 경로를 반환합니다.");
-            int safety_score = getSafetyScore(routePoints, filteredSafetyFacPoints, filteredDangerPoints);
-            return SafeRouteResponseDto.builder().detourRoute(originRoute).safety_score(safety_score).build();
+            safety_score = calcSafetyScore(routePoints, allSafetyPoints, allDangerPoints);
+            return buildResponse(originRoute, originRoute, safety_score); // 2차 기본 경로 리턴
         }
+
+        RouteResponseDto detourRoute = tmapRouteSvc.getDetourRoute(routeRequestDto, detourPoints); // ++추가 TmapAPI 호출
 
         System.out.println("우회 경유지 목록: " + Arrays.deepToString(detourPoints.toArray()));
         System.out.println("우회 경유지 크기: " + detourPoints.size() );
@@ -120,57 +147,44 @@ public class SafeRouteService {
         System.out.println("우회 경로 총 거리: " + detourRoute.getTotalDistance());
 
         double detourRatio = (double)detourRoute.getTotalDistance() / originRoute.getTotalDistance();
-        // 우회 경유지가 기존 경로보다 300m가 넓다면
+        // 우회 경유지가 기존 경로보다 1.2배 길다면
         if(  detourRatio > MAX_DETOUR_RATIO ){
             // 위험경로를 한 번만 우회하는 경로 생성
-            DetourWayPointDto singleWayPoint = detourRouteSvc.createSingleDetourWayPoint(routePoints, filteredDangerPoints.get(0) );
+            DetourWayPointDto singleWayPoint = detourRouteSvc.createSingleDetourWayPoint(routePoints, riskFilterList.get(0) );
             RouteResponseDto singleDetourRoute = tmapRouteSvc.getDetourRoute(routeRequestDto, List.of(singleWayPoint)); // 싱글 우회경로 생성
-            List<RoutePointDto> singleRoutePoints = singleDetourRoute.getRoutePoints();
-
-            System.out.println("한번 더 우회한 경로 총 시간: " + singleDetourRoute.getTotalTime());
-            System.out.println("한번 더 우회한 경로 총 거리: " + singleDetourRoute.getTotalDistance());
 
             // 한 번 더 우회했지만 비율이 여전히 20%가 넘으면 기본 경로로 반환
             double detourRatio2 = (double)singleDetourRoute.getTotalDistance() / originRoute.getTotalDistance();
             if( detourRatio2 > MAX_DETOUR_RATIO){
                 System.out.println("한 번 더 우회했지만 거리가 멉니다. 기본경로를 반환합니다.");
-                int safety_score = getSafetyScore(routePoints, filteredSafetyFacPoints, filteredDangerPoints);
-                return SafeRouteResponseDto.builder().detourRoute(originRoute).safety_score(safety_score).build();
+                safety_score = calcSafetyScore(routePoints, allSafetyPoints, allDangerPoints);
+                return buildResponse(originRoute, originRoute, safety_score); // 3차 기본 경로 리턴
             }
 
-            // 안전 지역 1,2차 필터링 ( bbox, 경로상 50m 이내 )
-            List<SafetyFacPointDto> singleFilteredSafetyFacPoints = safeFacFilterSvc.filterSafetyFacPoints(singleRoutePoints, allSafetyPoints);
-            // 위험 리스트 1, 2, 3차 필터링( bbox, 경로상 50m 이내, 연속된 위험지역 건너뛰기 )
-            List<RiskPointDto> singleFilteredDangerPoints = riskFilterSvc.filterDangerPoints(singleRoutePoints, allDangerPoints);
-
-            //한 번 더 필터링된 안전 지역, 위험 리스트
-            System.out.println("한 번 더 우회한 경로의 안전 시설물Dto: " + singleFilteredSafetyFacPoints);
-            System.out.println("한 번 더 우회한 겨올의 위험Dto: " + singleFilteredDangerPoints);
-
             // 싱글 경유지 우회 경로 안전 점수
-            int safety_score = getSafetyScore(singleDetourRoute.getRoutePoints(), singleFilteredSafetyFacPoints, singleFilteredDangerPoints);
-            return SafeRouteResponseDto.builder().detourRoute(singleDetourRoute).safety_score(safety_score).build();
+            safety_score = calcSafetyScore(singleDetourRoute.getRoutePoints(), allSafetyPoints, allDangerPoints);
+            return buildResponse(singleDetourRoute, originRoute, safety_score); // 4차 단일 우회경로 리턴
         }
 
-        // 안전 지역 1,2차 필터링 ( bbox, 경로상 50m 이내 )
-        List<SafetyFacPointDto> detourFilteredSafetyFacPoints = safeFacFilterSvc.filterSafetyFacPoints(detourRoutePoints, allSafetyPoints);
-        // 위험 리스트 1, 2, 3차 필터링( bbox, 경로상 50m 이내, 연속된 위험지역 건너뛰기 )
-        List<RiskPointDto> detourFilteredDangerPoints = riskFilterSvc.filterDangerPoints(detourRoutePoints, allDangerPoints);
-
-        System.out.println("우회한 경로의 안전시설물Dto: " + detourFilteredSafetyFacPoints);
-        System.out.println("우회한 경로의 위험Dto: " + detourFilteredDangerPoints);
 
         // 여러 경유지 우회 경로 안전 점수
-        int safety_score = getSafetyScore(detourRoute.getRoutePoints(), detourFilteredSafetyFacPoints, detourFilteredDangerPoints);
-        return SafeRouteResponseDto.builder()
-                .detourRoute(detourRoute).safety_score(safety_score).build();
+        safety_score = calcSafetyScore(detourRoute.getRoutePoints(), allSafetyPoints, allDangerPoints );
+        return buildResponse(detourRoute, originRoute, safety_score); // 5차 우회 경로 리턴
     }
 
     // 안전 점수 계산 로직: 경로(우회경로 or 기본 경로) 상의 위험지역 개수와 안전시설물의 개수를 따진다. 안전시설물은 어떤 안전시설물인지에 따라 차등을 다르게 둔다.
-    private int getSafetyScore( List<RoutePointDto> routePoints, List<SafetyFacPointDto> safetyFacPoints, List<RiskPointDto> dangerPoints ){
+    private int calcSafetyScore( List<RoutePointDto> routePoints, List<SafetyFacPointDto> allSafetyFacPoints, List<RiskPointDto> allDangerPoints ){
+        // 경로에 대해서 안전 시설물과 위험시설물에 대하여 가져온다,
+
+        List<SafetyFacPointDto> safetyFacPoints = safeFacFilterSvc.filterSafetyFacPoints(routePoints, allSafetyFacPoints);
+        List<RiskPointDto> riskPoints = riskFilterSvc.filterDangerPoints(routePoints, allDangerPoints);
+
+        System.out.println("우회한 경로의 안전시설물Dto: " + safetyFacPoints);
+        System.out.println("우회한 경로의 위험Dto: " + riskPoints);
+
         // 각 위치들의 개수 합치기 (안전 시설물은 CCTV/보안등, 경찰서, 안전지킴이집, 안전벨
         int safeCount = safetyFacPoints.stream().mapToInt(safeFac -> getFacScore(safeFac.getFacType()) * safeFac.getSafeCount() ).sum();
-        int riskCount = dangerPoints.stream().mapToInt(RiskPointDto::getRiskCount).sum() * -7;
+        int riskCount = riskPoints.stream().mapToInt(RiskPointDto::getRiskCount).sum() * -7;
 
         System.out.println("안전점수(보안등1점, CCTV/안전벨2점, 안전지킴이집4점, 경찰서5점): " + safeCount);
         System.out.println("위험점수(위험경로당-7점): " + riskCount);
