b44ea4b8e2eef77d926ef96f54f420121cd39ed4
leehyeon2021 <leehyeon2021@gmail.com>
2026-03-30 18:17:34 +0900

    260330 주석 삭제

--- src/main/java/iriro/publicData/service/RefreshAllDataService.java
+++ src/main/java/iriro/publicData/service/RefreshAllDataService.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class RefreshAllDataService {
-
-// 저장/조회 클래스 분리했으므로 아래 수정 필요
     private final FacilitySafeFetchService ffs;
     private final CrimeRoadFetchService cfs;
 
@@ -16,20 +14,12 @@ public class RefreshAllDataService {
     //@Scheduled(cron = "0 0 1 1 * *")  // 매월 1일 새벽 2시
     public boolean refreshAllData() {
 
-//        // 전체 저장 (자동 업데이트o)
-//            // sleep 같은 거 해야할 것 같음
-//        ffs.fetchPoliceStation();    // 경찰서
-//        ffs.fetchSafeHouse();        // 안심지킴이집
-//        ffs.fetchSafeFac();          // 안전시설물(보안등,CCTV,안전벨)
-//        cfs.fetchCrimeRoad();        // 위험도로명 - 가장 오래 걸림
-
-        // 1. 결과와 상관없이 일단 4개의 업데이트 메서드를 모두 실행합니다.
         boolean isPoliceUpdated = ffs.fetchPoliceStation();
         boolean isSafeHouseUpdated = ffs.fetchSafeHouse();
         boolean isSafeFacUpdated = ffs.fetchSafeFac();
         boolean isCrimeRoadUpdated = cfs.fetchCrimeRoad();
 
-        // 2. 4개가 모두 성공했는지(true) 확인해서 반환합니다.
+        // 2. 4개 모두 true 확인 반환.
         return isPoliceUpdated && isSafeHouseUpdated && isSafeFacUpdated && isCrimeRoadUpdated;
 
     }
