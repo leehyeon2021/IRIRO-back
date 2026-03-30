7ab0460c6d9f523da409c330648578054db5fa35
so0hub <so0mile02@gmail.com>
2026-03-30 16:14:13 +0900

    Optional 벗기기

--- src/main/java/iriro/community/repository/UserRepository.java
+++ src/main/java/iriro/community/repository/UserRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Integer> {
-    Optional<UserEntity> findByEmail(String email);
+    UserEntity findByEmail(String email);
 }
