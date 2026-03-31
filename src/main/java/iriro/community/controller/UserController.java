2ffd1e4e29ab6dae0d63bffcdc0fe88e289a57ac
so0hub <so0mile02@gmail.com>
2026-03-31 15:52:54 +0900

    마이페이지 등록 로직 수정

--- src/main/java/iriro/community/controller/UserController.java
+++ src/main/java/iriro/community/controller/UserController.java
@@ -45,8 +45,8 @@ public class UserController {
             String token = jwtService.createToken(loginDto.getEmail()); // 로그인성공 아이디를 토큰에 저장
             return ResponseEntity.ok()
                     // 토큰! 서버저장X 클라이언트에 저장O
-                            .header("Authorization","Bearer "+token) // 인증정보 담는 구역
-            // 클라이언트에게 헤더에 발급받은 jwt 토큰 반환한다.
+                    .header("Authorization", "Bearer " + token) // 인증정보 담는 구역
+                    // 클라이언트에게 헤더에 발급받은 jwt 토큰 반환한다.
                     .body(true); // 성공
         }
         // 3] 아니면 실패
@@ -56,7 +56,7 @@ public class UserController {
     // 3. 로그아웃 Get
     @GetMapping("/logout")
     // http://localhost:8080/user/logout?userId=11
-    public ResponseEntity<?> logout(){
+    public ResponseEntity<?> logout() {
         // 서버는 할 게 없지만 프론트에게 지워도 된다고 신호를 줌...
         return ResponseEntity.ok(true);
     }
@@ -65,14 +65,17 @@ public class UserController {
     // 4. 마이페이지
     @GetMapping("/myinfo")
     // http://localhost:8080/user/myinfo
-    public ResponseEntity<?> myInfo(@RequestHeader(value = "Authorization",required = false) String token){
-        if(token == null || !token.startsWith("Bearer ")){
+    public ResponseEntity<?> myInfo(@RequestHeader(value = "Authorization") String token) {
+        if (token == null || !token.startsWith("Bearer")) {
             return ResponseEntity.ok(false);
         }
-        token = token.replace("Bearer ","");
+        token = token.replace("Bearer ", "");
         String email = jwtService.getClaim(token);
-        if(email == null)return ResponseEntity.ok(false);
-        return ResponseEntity.ok(userService.myInfo(email));
+        UserDto result = userService.myInfo(email);
 
-        }
+        if (result == null) {return ResponseEntity.status(404).body("회원 정보가 없습니다.");
+    }
+
+        return ResponseEntity.ok(result);
+    }
 }
