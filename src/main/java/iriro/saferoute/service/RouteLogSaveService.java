4408fd1ee2c925898b0a3cf23b39e824f906991a
bm1611 <bm1611@naver.com>
2026-03-31 15:58:05 +0900

    컨틀롤러에서 로그 저장 서비스를 호출

--- src/main/java/iriro/saferoute/service/RouteLogSaveService.java
+++ src/main/java/iriro/saferoute/service/RouteLogSaveService.java
@@ -1,8 +1,12 @@
 package iriro.saferoute.service;
 
+import iriro.common.exception.EmailNotFoundException;
 import iriro.common.exception.LogSaveException;
 import iriro.community.entity.UserEntity;
+import iriro.community.repository.UserRepository;
 import iriro.saferoute.dto.RoutePointDto;
+import iriro.saferoute.dto.RouteResponseDto;
+import iriro.saferoute.dto.SafeRouteResponseDto;
 import iriro.saferoute.dto.SaveLogDto;
 import iriro.saferoute.entity.LocationlogEntity;
 import iriro.saferoute.entity.RoutePointLogEntity;
@@ -22,16 +26,50 @@ import java.util.List;
 @Transactional
 @Slf4j
 public class RouteLogSaveService {
+    private static final Integer GUEST_USER_ID = 1; // 비회원 user_id
+
     // 사용자 로그 저장하는 서비스
     private final LocationLogRepository locationLogRepo;
     private final RoutePointLogRepository routePointLogRepo;
+    private final UserRepository userRepo;
+
+    // 로그를 후기(평점)제외하고 저장하고 lodId를 반환하는 함수 --> API 명세서에 추가해야함
+    public Long saveRouteLog(SafeRouteResponseDto safeRouteResponse, String email ){
+
+        RouteResponseDto originRoute = safeRouteResponse.getDetourRoute();
+        int safety_score = safeRouteResponse.getSafety_score();
+
+        // SaveLogDto로 변환
+        SaveLogDto saveLog = originRoute.toSaveLogDto();
+        log.info("saveLogDto = {}", saveLog);
+        saveLog.setSafetyScore(safety_score); //안전 점수 추가
+
+        // 로그 1차 저장 후 로그 Id 받아오기
+        Long logId = createRouteLog(saveLog, email);
+        // 로그Id 경로까지 저장 후 잘 저장되었는지 반환하기 ( 로그 아이디와, 경로 배열을 반환)
+        List<RoutePointDto> safeRoutePoints = originRoute.getRoutePoints();
+        boolean result = saveLogRoute( logId, safeRoutePoints );
+
+        if(result) return logId; // 경로 저장까지 성공 시 로그아이디 반환
+        else return 0L; // 실패시 0L 반환
+    }
 
     // 경로 추천 시 일단 경로 제외 로그에 저장
-    public Long createRouteLog(SaveLogDto saveLogDto){
+    public Long createRouteLog(SaveLogDto saveLogDto, String email){
         log.info("RouteLogSaveService.createRouteLog saveLogDto = {}", saveLogDto);
         //후기와 사용자 빼고 다 저장 가능.
         LocationlogEntity saveEntity = saveLogDto.toEntity();
-        saveEntity.setUserEntity( UserEntity.builder().userId(1).build() );
+        UserEntity saveUser;
+        // 이메일 확인
+        if(email == null || email.isBlank()){
+            saveUser = UserEntity.builder().userId(GUEST_USER_ID).build();
+        }else{ // 이메일이 있으면
+            saveUser = userRepo.findByEmail(email)
+                    .orElseThrow(() -> new EmailNotFoundException("email is not found"));
+        }
+
+        saveEntity.setUserEntity( saveUser ); // 유저엔티티 저장
+
         return locationLogRepo.save( saveEntity ).getLogId();
     }
 
@@ -58,7 +96,7 @@ public class RouteLogSaveService {
 
     // 후기(별점)을 받으면 추가로 저장 --> update에 가깝다.
     public boolean updateLogRating(Long logId, Integer rating){
-        // 후기 저장, 사용자 저장 (update하기)
+        // 후기 저장
         return true;
     }
 
