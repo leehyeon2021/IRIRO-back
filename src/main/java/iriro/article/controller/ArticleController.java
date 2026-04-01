57aceca3a7ab89e0b41eb30e78bccfa5993827cb
leehyeon2021 <leehyeon2021@gmail.com>
2026-04-01 21:58:32 +0900

    260401 테스트 크롤링

--- src/main/java/iriro/article/controller/ArticleController.java
+++ src/main/java/iriro/article/controller/ArticleController.java
@@ -1,17 +1,22 @@
 package iriro.article.controller;
 
+import iriro.article.service.ArticleCrawlingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
-@RequestMapping("/articles")
 public class ArticleController {
 
-//    @GetMapping("/find")
-//    public ResponseEntity<?> getArticleFindAll(){
-//        return ResponseEntity.ok(as.getArticleFindAll());
-//    }
+    private final ArticleCrawlingService crawlingService;
 
+    @GetMapping("/crawl")
+    public String startCrawlingTest() {
+        System.out.println("크롤링 수동 시작");
+        crawlingService.crawlAll();
+        return "크롤링 테스트가 완료";
+    }
 }
