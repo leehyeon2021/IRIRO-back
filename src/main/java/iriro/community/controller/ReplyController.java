7b27156c931bea63b717de4c8c97abaeb5d0e397
so0hub <so0mile02@gmail.com>
2026-04-02 16:48:44 +0900

    ReplyController @CrossOrigin 추가 및 주소 수정

--- src/main/java/iriro/community/controller/ReplyController.java
+++ src/main/java/iriro/community/controller/ReplyController.java
@@ -10,19 +10,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
-@RequestMapping("/board")
+@RequestMapping("/api/board")
 public class ReplyController {
 
-    @Autowired
     private final ReplyService replyService;
     private final JWTService jwtService;
 
     // 1. 댓글 등록
-    // http://localhost:8080/board/rpwrite
+    // http://localhost:8080/api/board/rp
     // Bearer eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNvc29AbmF2ZXIuY29tIiwiaWF0IjoxNzc0OTM3NjY1LCJleHAiOjE3NzUwMjQwNjV9.e2i3Yl9CpiEjvSH-uAwxy1pINyKvrbzHt0XNGwPS7Ws
     // { "replyContent" : "박진감보고싶습니감ㅠㅠ" , "boardId" : 2 }
-    @PostMapping("/rpwrite")
+    @PostMapping("/rp")
     public ResponseEntity<?> rpAdd(@RequestBody ReplyDto replyDto, @RequestHeader(value = "Authorization",required = false) String token) {
 
         // 기본값=비회원 이메일
@@ -40,7 +40,7 @@ public class ReplyController {
     }
 
     // 2. 댓글 삭제
-    // http://localhost:8080/board/rpdelete?replyId=1
+    // http://localhost:8080/api/board/rpdelete?replyId=1
     @DeleteMapping("/rpdelete")
     public ResponseEntity<?> rpDelete(@RequestParam Integer replyId,
                                       @RequestHeader(value = "Authorization",required = false)String token) {
