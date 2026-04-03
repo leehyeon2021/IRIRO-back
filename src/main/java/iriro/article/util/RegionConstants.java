6363b67980a654155c97c1c9abe2effada9f47f2
leehyeon2021 <leehyeon2021@gmail.com>
2026-04-03 15:58:18 +0900

    260403 [크롤링] test

--- src/main/java/iriro/article/util/RegionConstants.java
+++ src/main/java/iriro/article/util/RegionConstants.java
@@ -3,14 +3,14 @@ package iriro.article.util;
 // 상수 전용 클래스
 public class RegionConstants {
 
-    // 1. 서울시 25개 구 명단 (배열)
-    public final static String[] seoulDistricts = {
-            "강남구", "강동구", "강북구", "강서구", "관악구", "광진구", "구로구", "금천구",
-            "노원구", "도봉구", "동대문구", "동작구", "마포구", "서대문구", "서초구", "성동구",
-            "성북구", "송파구", "양천구", "영등포구", "용산구", "은평구", "종로구", "중구", "중랑구"
-    };
+//    // 1. 서울시 25개 구 명단 (배열)
+//    public final static String[] seoulDistricts = {
+//            "강남구", "강동구", "강북구", "강서구", "관악구", "광진구", "구로구", "금천구",
+//            "노원구", "도봉구", "동대문구", "동작구", "마포구", "서대문구", "서초구", "성동구",
+//            "성북구", "송파구", "양천구", "영등포구", "용산구", "은평구", "종로구", "중구", "중랑구"
+//    };
 
     // 테스트용
-    // public static final String[] seoulDistricts = {"강남구", "종로구"};
+    public static final String[] seoulDistricts = {"종로구"};
 
 }
