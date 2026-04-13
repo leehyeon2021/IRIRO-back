1c0a690698e9cd858af519f1fce71021b5f52960
so0hub <so0mile02@gmail.com>
2026-04-13 12:34:16 +0900

    역지오코딩 컨트롤러 CrossOrigin 추가

--- src/main/java/iriro/community/controller/ReverseGeocodingController.java
+++ src/main/java/iriro/community/controller/ReverseGeocodingController.java
@@ -1,12 +1,14 @@
 package iriro.community.controller;
 
 import iriro.community.service.ReverseGeocodingService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class ReverseGeocodingController {
     private final ReverseGeocodingService reverseGeocodingService;
