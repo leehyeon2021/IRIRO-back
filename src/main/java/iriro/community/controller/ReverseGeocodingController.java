630c8bcaf0c946a97b2009c98bdb8ea71cf91090
so0hub <so0mile02@gmail.com>
2026-04-13 11:42:31 +0900

    주소변환 역지오코딩 기능 테스트 컨트롤러

---
+++ src/main/java/iriro/community/controller/ReverseGeocodingController.java
@@ -0,0 +1,21 @@
+package iriro.community.controller;
+
+import iriro.community.service.ReverseGeocodingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReverseGeocodingController {
+    private final ReverseGeocodingService reverseGeocodingService;
+
+    // 생성자로 서비스 주입받기
+    public ReverseGeocodingController(ReverseGeocodingService reverseGeocodingService){
+        this.reverseGeocodingService = reverseGeocodingService;
+    }
+
+    @GetMapping("/api/test-address")
+    public String testAddress(@RequestParam String x , @RequestParam String y){
+        return reverseGeocodingService.findAddress(x,y);
+    }
+}
