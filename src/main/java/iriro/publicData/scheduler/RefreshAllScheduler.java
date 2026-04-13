c2fbd9aed7ade964a15f006e8995297b3b1311f2
leehyeon2021 <leehyeon2021@gmail.com>
2026-04-13 15:27:50 +0900

    260413 공공데이터 수집 확인용 print 추가

--- src/main/java/iriro/publicData/scheduler/RefreshAllScheduler.java
+++ src/main/java/iriro/publicData/scheduler/RefreshAllScheduler.java
@@ -16,10 +16,22 @@ public class RefreshAllScheduler {
     @Scheduled(cron = "0 0 1 1 * *")  // 매월 1일 새벽 2시
     public boolean refreshAllData() {
 
+
+        System.out.println("========== [경찰서 수집 시작] ==========");
         boolean isPoliceUpdated = ffs.fetchPoliceStation();
+        System.out.println("========== [경찰서 수집 결과] " + isPoliceUpdated + " ==========");
+
+        System.out.println("========== [안전지킴이집 수집 시작] ==========");
         boolean isSafeHouseUpdated = ffs.fetchSafeHouse();
+        System.out.println("========== [안전지킴이집 수집 결과] "+isSafeHouseUpdated+" ==========");
+
+        System.out.println("========== [안전시설물 수집 시작] ==========");
         boolean isSafeFacUpdated = ffs.fetchSafeFac();
+        System.out.println("========== [안전시설물 수집 결과] "+isSafeFacUpdated+" ==========");
+
+        System.out.println("========== [위험도로명 수집 시작] ==========");
         boolean isCrimeRoadUpdated = cfs.fetchCrimeRoad();
+        System.out.println("========== [위험도로명 수집 결과] "+isCrimeRoadUpdated+" ==========");
 
         // 2. 4개 모두 true 확인 반환.
         return isPoliceUpdated && isSafeHouseUpdated && isSafeFacUpdated && isCrimeRoadUpdated;
