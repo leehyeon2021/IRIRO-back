94e2ee1b5f8baecd87509079270bcbaa6e2698e5
so0hub <so0mile02@gmail.com>
2026-04-13 15:25:32 +0900

    댓글상세조회변경

--- src/main/java/iriro/community/repository/ReplyRepository.java
+++ src/main/java/iriro/community/repository/ReplyRepository.java
@@ -4,7 +4,10 @@ import iriro.community.entity.ReplyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface ReplyRepository extends JpaRepository<ReplyEntity,Integer> {
+    List<ReplyEntity> findByBoardEntity_BoardId(Integer boardId);
 }
